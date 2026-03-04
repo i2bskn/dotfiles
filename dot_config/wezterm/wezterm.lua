325b4022d5edbeb96aa01b8460434776e7828884
i2bskn <i2bskn@gmail.com>
2026-03-04 12:10:39 +0900

    Fix font

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,7 +2,10 @@ local wezterm = require("wezterm")
 local config = {}
 
 -- Font
-config.font = wezterm.font("JetBrains Mono")
+config.font = wezterm.font_with_fallback({
+	"JetBrains Mono",
+	"Hiragino Kaku Gothic ProN",
+})
 config.font_size = 13.0
 
 -- Color Scheme
